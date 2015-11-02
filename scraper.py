4191913fa32f695d377552591b31b58806c04012
blablupcom <olegvolodin@eyahoo.com>
2015-11-02 20:53:39 +0300

    first commit

--- scraper.py
+++ scraper.py
@@ -6,7 +6,6 @@ import os
 import re
 import scraperwiki
 import urllib2
-import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -38,25 +37,6 @@ def validateFilename(filename):
 
 
 def validateURL(url):
-     # try:
-     #    r = requests.get(url, allow_redirects=True, timeout=20)
-     #    count = 1
-     #    while r.status_code == 500 and count < 4:
-     #        print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
-     #        count += 1
-     #        r = requests.get(url, allow_redirects=True, timeout=20)
-     #    sourceFilename = r.headers.get('Content-Disposition')
-     #    if sourceFilename:
-     #        ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
-     #    else:
-     #        ext = os.path.splitext(url)[1]
-     #    validURL = r.status_code == 200
-     #    validFiletype = ext in ['.csv', '.xls', '.xlsx']
-     #    return validURL, validFiletype
-     # except:
-     #    print ("Error validating URL.")
-     #    return False, False
-
     try:
         r = urllib2.urlopen(url)
         count = 1
